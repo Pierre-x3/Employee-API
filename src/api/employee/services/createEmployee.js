540b701b6b4f40c4dcbb5028157afed7b0669462
Pierre Bernaola <pieerre95x3@gmail.com>
2024-07-20 12:42:07 -0500

    error column create

--- src/api/employee/services/createEmployee.js
+++ src/api/employee/services/createEmployee.js
@@ -3,7 +3,7 @@ const { getSQLiteDB } = require("../../../database/SQLite");
 const createEmployee = async ({ name, lastname, age }) => {
   const db = await getSQLiteDB();
   return await db.get(`
-    INSERT INTO employee (name, lastname, age) VALUES (${name}, ${lastname}, ${age})
+    INSERT INTO employee (name, lastname, age) VALUES ("${name}", "${lastname}", "${age}")
   `);
 }
 
